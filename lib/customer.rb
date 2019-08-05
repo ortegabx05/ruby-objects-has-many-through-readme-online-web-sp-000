1e056b409cd2e1b1276dd1506a061a5cd70de35f
jessi ortega <ortegabx05@gmail.com>
2019-08-05 21:47:29 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -3,22 +3,30 @@ class Customer
  
   @@all = []
  
-  def initialize(name, age)
+ def initialize(name, age)
     @name = name
     @age = age
     @@all << self
-  end
+ end
  
-  def self.all
+ def self.all
     @@all
-  end
-   def new_meal(waiter, total, tip=0)
+ end
+ 
+ def new_meal(waiter, total, tip=0)
     Meal.new(waiter, self, total, tip)
-  end
+ end
  
  def meals
   Meal.all.select do |meal|
     meal.customer == self
+   end
+ end
+
+ def waiters
+  meals.map do |meal|
+    meal.waiter
   end
-end
+ end
+
 end
